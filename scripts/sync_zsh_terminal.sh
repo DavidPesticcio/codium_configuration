383e2f7f0640aa9b8524b8aba0567f9382297af8
fastcode.codingzen <fastcode.codingzen@gmail.com>
2022-04-19 17:17:10 +0700

    update zsh history command

--- scripts/sync_zsh_terminal.sh
+++ scripts/sync_zsh_terminal.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 while true; do
-    read -p "Do you want to copy configuration to repository? [Y/n] " yn
+    read -p "Do you want to copy ZSH configuration to repository? [Y/n] " yn
     case $yn in
         [Yy]* ) cp -u "${HOME}/.zshrc" "${REPO_DIRECTORY}/zsh/dev/.zshrc"; cp -u "${HOME}/.zsh_history" "${REPO_DIRECTORY}/zsh/dev/.zsh_history"; break;;
         [Nn]* ) exit;;
