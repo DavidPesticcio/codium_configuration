17c7e86560eb3a6f9810d2715ba30d17e2c00ae8
kris <kris@localhost.localdomain>
2022-05-24 21:16:53 +0700

    sync codium settings

--- scripts/sync_codium.sh
+++ scripts/sync_codium.sh
@@ -11,7 +11,7 @@ while true; do
             while true; do
                 read -p "Confirm [Y/n]:" yn
                 case $yn in
-                    [Yy]* )  cp -vur "${CODIUM_USER_CONFIG_PATH}/snippets/"* "${REPO_DIRECTORY}/Codium/User/snippets" ; cp -u "${CODIUM_USER_CONFIG_PATH}/keybindings.json" "${REPO_DIRECTORY}/Codium/User/keybindings.json"; cp -u "${CODIUM_USER_CONFIG_PATH}/settings.json" "${REPO_DIRECTORY}/Codium/User/settings.json" ; echo "##### DONE" ;
+                    [Yy]* )  cp -vur "${CODIUM_USER_CONFIG_PATH}/snippets/"* "${REPO_DIRECTORY}/Codium/User/snippets/" ; cp -u "${CODIUM_USER_CONFIG_PATH}/keybindings.json" "${REPO_DIRECTORY}/Codium/User/keybindings.json"; cp -u "${CODIUM_USER_CONFIG_PATH}/settings.json" "${REPO_DIRECTORY}/Codium/User/settings.json" ; echo "##### DONE" ;
                         break;;
 
                     [Nn]* ) exit;;
