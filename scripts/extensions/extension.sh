e12ca329200b52fa62304d44910dc3ec21e89f68
vanvuvuong@gmail.com <kris@localhost.localdomain>
2022-07-07 21:37:44 +0700

    sync codium & zsh configuration

--- scripts/extensions/extension.sh
+++ scripts/extensions/extension.sh
@@ -1,14 +1,17 @@
 codium --install-extension 4ops.terraform
 codium --install-extension bmewburn.vscode-intelephense-client
 codium --install-extension christian-kohler.npm-intellisense
+codium --install-extension dsznajder.es7-react-js-snippets
 codium --install-extension eamodio.gitlens
 codium --install-extension EditorConfig.EditorConfig
 codium --install-extension foxundermoon.shell-format
+codium --install-extension HookyQR.beautify
 codium --install-extension ms-azuretools.vscode-docker
 codium --install-extension ms-pyright.pyright
 codium --install-extension ms-vscode.live-server
 codium --install-extension ms-vscode.sublime-keybindings
 codium --install-extension mtxr.sqltools
+codium --install-extension njpwerner.autodocstring
 codium --install-extension PKief.material-icon-theme
 codium --install-extension redhat.vscode-yaml
 codium --install-extension sourcery.sourcery
