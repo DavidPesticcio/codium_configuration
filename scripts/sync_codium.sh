cb9848acb0c936515c8599bc67fd1cf0e3270d66
vanvuvuong@gmail.com <vanvuvuong3@gmail.com>
2022-11-10 09:45:23 +0700

    format script, update setting of old codium version: codium_1.67.0-1651928812_amd64

--- scripts/sync_codium.sh
+++ scripts/sync_codium.sh
@@ -7,32 +7,45 @@ while true; do
     4.Export Extension to scripts
     --Choose 1, 2, 3 or 4: ' option
     case $option in
-        [1] )
-            while true; do
-                read -p "Confirm [Y/n]:" yn
-                case $yn in
-                    [Yy]* )  cp -vur "${CODIUM_USER_CONFIG_PATH}/snippets/"* "${REPO_DIRECTORY}/Codium/User/snippets/" ; cp -u "${CODIUM_USER_CONFIG_PATH}/keybindings.json" "${REPO_DIRECTORY}/Codium/User/keybindings.json"; cp -u "${CODIUM_USER_CONFIG_PATH}/settings.json" "${REPO_DIRECTORY}/Codium/User/settings.json" ; echo "##### DONE" ;
-                        break;;
+    [1])
+        while true; do
+            read -p "Confirm [Y/n]:" yn
+            case $yn in
+            [Yy]*)
+                cp -vur "${CODIUM_USER_CONFIG_PATH}/snippets/"* "${REPO_DIRECTORY}/Codium/User/snippets/"
+                cp -u "${CODIUM_USER_CONFIG_PATH}/keybindings.json" "${REPO_DIRECTORY}/Codium/User/keybindings.json"
+                cp -u "${CODIUM_USER_CONFIG_PATH}/settings.json" "${REPO_DIRECTORY}/Codium/User/settings.json"
+                echo "##### DONE"
+                break
+                ;;
 
-                    [Nn]* ) exit;;
-                esac
-            done
-            break;;
-        [2] )
-            while true; do
-                read -p "Confirm [Y/n]:" yn
-                case $yn in
-                    [Yy]* )  cp -vur "${REPO_DIRECTORY}/Codium/User/"* "${CODIUM_USER_CONFIG_PATH}";
-                        break;;
-                    [Nn]* ) exit;;
-                esac
-            done
-            break;;
-        [3] )
-            echo "Install Extension to Codium"; exec "${REPO_DIRECTORY}/scripts/extensions/extension.sh";
-            break;;
-        [4] )
-            echo "Export Codium Extensions installation script"; codium --list-extensions  | xargs -L 1 echo codium --install-extension |& tee ${REPO_DIRECTORY}/scripts/extensions/extension.sh;
-            break;;
+            [Nn]*) exit ;;
+            esac
+        done
+        break
+        ;;
+    [2])
+        while true; do
+            read -p "Confirm [Y/n]:" yn
+            case $yn in
+            [Yy]*)
+                cp -vur "${REPO_DIRECTORY}/Codium/User/"* "${CODIUM_USER_CONFIG_PATH}"
+                break
+                ;;
+            [Nn]*) exit ;;
+            esac
+        done
+        break
+        ;;
+    [3])
+        echo "Install Extension to Codium"
+        exec "${REPO_DIRECTORY}/scripts/extensions/extension.sh"
+        break
+        ;;
+    [4])
+        echo "Export Codium Extensions installation script"
+        codium --list-extensions | xargs -L 1 echo codium --install-extension --force |& tee ${REPO_DIRECTORY}/scripts/extensions/extension.sh
+        break
+        ;;
     esac
 done
--- scripts/sync_zsh.sh
+++ scripts/sync_zsh.sh
@@ -5,17 +5,21 @@ while true; do
     2. From REPOSITORY to LOCAL
     Choose 1 or 2: " option
     case $option in
-        [1] )
-            read -p "Which User? (for example dev): " user
-            echo "### Sycing from LOCAL to REPO";
-            cp -u "${HOME}/.zshrc" "${REPO_DIRECTORY}/zsh/$user/.zshrc"; cp -u "${HOME}/.zsh_history" "${REPO_DIRECTORY}/zsh/$user/.zsh_history";
-            echo "### Done";
-            break;;
-        [2] )
-            read -p "Which User? (for example dev): " user
-            echo "### Syncing from REPO to LOCAL"
-            cp -u "${REPO_DIRECTORY}/zsh/$user/.zshrc" "${HOME}/.zshrc"; cp -u "${REPO_DIRECTORY}/zsh/$user/.zsh_history" "${HOME}/.zsh_history";
-            echo "### Done";
-            break;;
+    [1])
+        read -p "Which User? (for example dev): " user
+        echo "### Sycing from LOCAL to REPO"
+        cp -u "${HOME}/.zshrc" "${REPO_DIRECTORY}/zsh/$user/.zshrc"
+        cp -u "${HOME}/.zsh_history" "${REPO_DIRECTORY}/zsh/$user/.zsh_history"
+        echo "### Done"
+        break
+        ;;
+    [2])
+        read -p "Which User? (for example dev): " user
+        echo "### Syncing from REPO to LOCAL"
+        cp -u "${REPO_DIRECTORY}/zsh/$user/.zshrc" "${HOME}/.zshrc"
+        cp -u "${REPO_DIRECTORY}/zsh/$user/.zsh_history" "${HOME}/.zsh_history"
+        echo "### Done"
+        break
+        ;;
     esac
 done
